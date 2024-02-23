5c106adaf90dc76c94378cf7d0ff2eb36976c02a
wasim2002 <wansari4540@gmail.com>
2024-02-23 20:42:45 +0530

    write code for creating div for localstorage notes

--- script.js
+++ script.js
@@ -17,36 +17,41 @@ function createNote() {
     `
 
     main.appendChild(note)
-    saveNote()  
+    // saveNote() 
     let deleteNote = note.querySelector(".fa-trash-can")
     let saveBtn = note.querySelector(".fa-floppy-disk")
     deleteNote.addEventListener("click", function () {
         note.remove()
-        saveNote()
+        // saveNote()
     })
     saveBtn.addEventListener("click", saveNote)
 
 }
-
 function saveNote() {
     const allNotes = document.querySelectorAll(".textarea textarea")
-    let arr = []
+    let arr = JSON.parse(localStorage.getItem("notes")) || []
+    // console.log(`1 - ${arr}`);
     allNotes.forEach((noteData) => {
         arr.push(noteData.value.trim())
     })
+    // console.log(`2 - ${arr}`);
     localStorage.setItem("notes", JSON.stringify(arr))
 }
+(function createNoteLs() {
+    let storedNotes = JSON.parse(localStorage.getItem("notes")) || []
+    storedNotes.forEach((storeNote) => {
+        let note = document.createElement("div")
+        note.classList.add("note")
+        note.innerHTML = `
+    <div class="note_nav">
+                <i class="fa-solid fa-floppy-disk" style="color: #ffffff;"></i>
+                <i class="fa-regular fa-trash-can" style="color: #ffffff;"></i>
+            </div>
+            <div class="textarea">
+                <textarea autofocus>${storeNote}</textarea>
+            </div>
+    `
 
-(
-    function () {
-        const lsNotes = JSON.parse(localStorage.getItem("notes"))
-        lsNotes.forEach((lsnote) => {
-            createNote(lsnote)
-        })
-        if (localStorage.length === 0) {
-            localStorage.removeItem("notes")
-        } else {
-            createNote()
-        }
-    }
-)()
+        main.appendChild(note)
+    })
+})()
