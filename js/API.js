558828c11d20cb50ae8fc4b8317f947926027400
wasim2002 <wansari4540@gmail.com>
2024-04-24 13:50:45 +0530

    create all API function as get,set and delete in localstorage

---
+++ js/API.js
@@ -0,0 +1,30 @@
+export default class NoteAPI {
+    static getLocalNotes() {
+        const localNotes = JSON.parse(localStorage.getItem("notes")) || []
+        return localNotes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+        })
+    }
+    static uniqueID() {
+        return new Date().getTime()
+    }
+    static saveNote(noteToSave) {
+        const lsNote = NoteAPI.getLocalNotes()
+        const existing = lsNote.find((note) => note.id == noteToSave.id)
+        if (existing) {
+            existing.title = noteToSave.title
+            existing.body = noteToSave.body
+            existing.updated = new Date().toISOString()
+        } else {
+            noteToSave.id = NoteAPI.uniqueID()
+            noteToSave.updated = new Date().toISOString()
+            lsNote.push(noteToSave)
+        }
+        localStorage.setItem("notes", JSON.stringify(lsNote))
+    }
+    static deleteNote(id) {
+        const lsNote = NoteAPI.getLocalNotes()
+        const newNote = lsNote.filter((note) => note.id != id)
+        localStorage.setItem("notes",JSON.stringify(newNote))
+    }
+}
