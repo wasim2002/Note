0f510ae68d279b38d31607965291a05f8d422dc1
wasim2002 <wansari4540@gmail.com>
2024-04-24 15:44:02 +0530

    add HTML in root to render on web through js file

--- js/notesView.js
+++ js/notesView.js
@@ -1,5 +1,26 @@
 export default class notesView {
-    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}){
+    constructor(root, { onNoteSelect, onNoteAdd, onNoteEdit, onNoteDelete } = {}) {
+        this.root = root;
+        this.onNoteSelect = onNoteSelect;
+        this.onNoteAdd = onNoteAdd;
+        this.onNoteEdit = onNoteEdit;
+        this.onNoteDelete = onNoteDelete;
+        this.root.innerHTML = `
+        <div class="list-sec">
+            <div class="btn">
+                <div class="add-btn">
+                    Add Note
+                </div>
+            </div>
+            <div class="list-items">
+            </div>
+        </div>
+        <hr>
+        <div class="preview-sec">
+        <input type="text" name="" id="body-title" placeholder="Title...">
+        <textarea class="textarea" placeholder="Note..."></textarea>
+        </div>
+        `
         
     }
 }
