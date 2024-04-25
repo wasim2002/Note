f3a50fbc8fb50721fcd7c60a61a059244af300dc
wasim2002 <wansari4540@gmail.com>
2024-04-25 10:23:02 +0530

    call the update function for printing all array from localStorage

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-import NoteAPI from "./API.js"
+import notesView from "./notesView.js";
+import NoteAPI from "./API.js";
+
+const main = document.querySelector("main")
+const view = new notesView(main, {
+    onNoteEdit(title, body) {
+    }
+})
+view.updateListItems(NoteAPI.getLocalNotes())
