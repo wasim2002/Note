8798a78c696c00910cfc2c90546037566358cd37
wasim2002 <wansari4540@gmail.com>
2024-04-25 13:46:02 +0530

    clear all code and create new file for those codes, now this is only connecter between all js and html,css files

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
-import notesView from "./notesView.js";
-import NoteAPI from "./API.js";
+import App from "./App"
 
 const main = document.querySelector("main")
-const view = new notesView(main, {
-    onNoteEdit(){
-
-    },
-    onNoteSelect(id) {
-        console.log(`note id : ${id}`);
-    },
-    onNoteDelete(id){
-        console.log("note deleted: " ,id);
-    }
-})
-view.updateListItems(NoteAPI.getLocalNotes())
+const App = new App(main)
