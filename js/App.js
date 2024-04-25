7fc18709bd3fe3929c286a86fbb1c3c7ad6ff902
wasim2002 <wansari4540@gmail.com>
2024-04-25 20:26:56 +0530

    write functions for geting from ls , printing list items and set active note

--- js/App.js
+++ js/App.js
@@ -1,10 +1,27 @@
 import notesView from "./notesView.js"
+import NoteAPI from "./API.js"
 
 export default class App {
     constructor(root) {
         this.notes = []
         this.activeNote = null
         this.view = new notesView(root, this.#handlers())
+        this.#refreshNotes()
+    }
+    #refreshNotes() {
+        const notes = NoteAPI.getLocalNotes()
+        this.#setNotes(notes)
+        if (notes.lenght > 0) {
+            this.#setActiveNote(notes[0])
+        }
+    }
+    #setNotes(notes) {
+        this.notes = notes
+        this.view.updateListItems(notes)
+    }
+    #setActiveNote(note) {
+        this.activeNote = note;
+        this.view.updateActiveNote(note)
     }
     #handlers() {
         return {
