923cc7c33a32165e24a17629e701bb6fb5593f73
wasim2002 <wansari4540@gmail.com>
2024-04-30 11:56:23 +0530

    change to list-item title tag with ellipsis

--- js/notesView.js
+++ js/notesView.js
@@ -17,13 +17,31 @@ export default class notesView {
         </div>
         <hr>
         <div class="preview-sec">
-        <input type="text" name="" id="body-title" placeholder="Title...">
-        <textarea class="textarea" placeholder="Note..."></textarea>
+            <div class="color">
+                <img src="https://www.w3schools.com/images/colorpicker2000.png" alt="">
+                <div class="bg-option">
+                    <li id="#E2DFD0" style="background-color: #E2DFD0;"></li>
+                    <li id="#FFD0D0" style="background-color: #FFD0D0;"></li>
+                    <li id="#FFFBDA" style="background-color: #FFFBDA;"></li>
+                    <li id="#E1F7F5" style="background-color: #E1F7F5;"></li>
+                    <li id="#F0EBE3" style="background-color: #F0EBE3;"></li>
+                    <li id="#E8EFCF" style="background-color: #E8EFCF;"></li>
+                    <li id="#BED7DC" style="background-color: #BED7DC;"></li>
+                    <li id="#F5E8DD" style="background-color: #F5E8DD;"></li>
+                    <li id="#E4DEBE" style="background-color: #E4DEBE;"></li>
+                </div>
+            </div>
+            <input type="text" name="" id="body-title" placeholder="Title...">
+            <textarea class="textarea" placeholder="Note..."></textarea>
         </div>
         `
         const btnAddNote = this.root.querySelector(".add-btn")
         const inputTitle = this.root.querySelector("#body-title")
         const inputBody = this.root.querySelector(".textarea")
+        const colorImg = this.root.querySelector(".color img")
+        const colorOptCont = this.root.querySelector(".bg-option")
+        const colorOpt = this.root.querySelectorAll("li")
+        const preSec = this.root.querySelector(".preview-sec")
 
         btnAddNote.addEventListener("click", () => {
             this.onNoteAdd();
@@ -37,11 +55,34 @@ export default class notesView {
             })
         });
         this.updatePreviewVisibility(false)
+
+        let flagForColorCheckout = false
+        colorImg.addEventListener("click", () => {
+            colorImg.classList.add("scaleOff");
+            colorOptCont.classList.add("scaleOn");
+            flagForColorCheckout = true
+        })
+        colorOpt.forEach((color) => {
+            color.addEventListener("click", (e) => {
+                const bgColor = e.target.id
+                // preSec.style.backgroundColor = bgColor
+            })
+        })
+        document.addEventListener("click", function (e) {
+            if (flagForColorCheckout) {
+                if (!colorImg.contains(e.target)) {
+                    colorImg.classList.remove("scaleOff");
+                colorOptCont.classList.remove("scaleOn")
+                    flagForColorCheckout = false
+                }
+            }
+        })
+
     }
     #createListItemHTML(id, title, body, updated) {
         return `
         <div class="list-item" id="${id}">
-        <input type="text" name="" id="list-title" placeholder="${title}" disabled>
+        <div id="list-title">${title}</div>
         <div class="body-info">${body}</div>
         <div class="time"><em>
         ${updated.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}
